ad6bac1e1a17ae3fabfd8b10f94f672ad7a95c06
YujiAzama <yuji.azama@gmail.com>
2016-07-27 19:53:08 +0900

    fix PWM class

--- pwm/pwm.py
+++ pwm/pwm.py
@@ -1,26 +1,26 @@
 import time
 
-#from RPi import GPIO
+from RPi import GPIO
+
+
+LOW = 0
+HIGH = 1
 
 
 class PWM(object):
 
     def __init__(self, pin):
         self.pin = pin
-#        GPIO.setmode(GPIO.BCM)
-#        GPIO.setup(pin, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
 
     def start(self, duty=50.0, freq=1000.0):
         while True:
-#            GPIO.output(self.pin, 1)
-            print "----+"
-            print "    |"
-            time.sleep(duty / freq / 100)
+            GPIO.output(self.pin, HIGH)
+            time.sleep(duty / freq / 100.0)
 
-#            GPIO.output(self.pin, 0)
-            print "+---+"
-            print "|"
-            time.sleep((100 - duty) / freq / 100)
+            GPIO.output(self.pin, LOW)
+            time.sleep((100.0 - duty) / freq / 100.0)
 
 
 if __name__ == "__main__":
